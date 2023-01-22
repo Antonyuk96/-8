db06fb3ab3d846d5224a9b48c28d93f169cafef0
Antonyuk96 <113938370+Antonyuk96@users.noreply.github.com>
2023-01-22 18:04:46 +0300

    Add files via upload

---
+++ exceptions.py
@@ -0,0 +1,254 @@
+Python 3.11.1 (tags/v3.11.1:a7a450f, Dec  6 2022, 19:58:39) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+def user_choice():
+   choice1 = input('Введите Ваш выбор: ')
+  try:
+      
+SyntaxError: unindent does not match any outer indentation level
+try:
+   choice1 = int(choice1)
+   if (choice1 > 5) and (choice1 < 0):
+if (choice1 > 8) and (choice1 < 0):
+    
+SyntaxError: expected an indented block after 'if' statement on line 3
+if (choice1 > 8) and (choice1 < 0):
+     print('Error')
+     return user_choice()
+    
+SyntaxError: incomplete input
+return user_choice()
+SyntaxError: 'return' outside function
+ return user_choice()
+SyntaxError: unexpected indent
+if (choice1 > 8) and (choice1 < 0):
+     print('Error')
+     return user_choice()
+    
+SyntaxError: incomplete input
+if (choice1 > 8) and (choice1 < 0):
+    print('Error')
+    error_enter()
+   return user_choice()
+SyntaxError: unindent does not match any outer indentation level
+if (choice1 > 5) and (choice1 < 0):
+    print('Error')
+    return user_choice()
+SyntaxError: incomplete input
+return user_choice()
+        else:
+            return choice1
+    except ValueError:
+        
+SyntaxError: multiple statements found while compiling a single statement
+if (choice1 > 5) and (choice1 < 0):
+    print('Error')
+    return user_choice()
+SyntaxError: incomplete input
+def user_choice():
+choice1 = input('Введите Ваш выбор: ')
+SyntaxError: expected an indented block after function definition on line 1
+def user_choice():
+    choice1 = input('Введите Ваш выбор: ')
+try:
+    
+SyntaxError: invalid syntax
+try:
+   choice1 = int(choice1)
+   if (choice1 > 8) and (choice1 < 0):
+       print('Error')
+       
+      return user_choice()
+    
+SyntaxError: unindent does not match any outer indentation level
+try:
+   choice1 = int(choice1)
+   if (choice1 > 5) and (choice1 < 0):
+     if (choice1 > 8) and (choice1 < 0):
+          print('Error')
+ return user_choice()
+SyntaxError: unindent does not match any outer indentation level
+try:
+    choice1 = int(choice1)
+    if (choice1 > 8) and (choice1 < 0):
+        print('Error')
+        return user_choice()
+    else:
+        return choice1
+    except ValueError:
+        
+SyntaxError: invalid syntax
+except ValueError:
+    
+SyntaxError: invalid syntax
+except ValueError:
+    
+SyntaxError: invalid syntax
+except ValueError:
+    
+SyntaxError: invalid syntax
+else:
+    
+SyntaxError: invalid syntax
+try:
+    choice1 = int(choice1)
+    if (choice1 > 8) and (choice1 < 0):
+        print('Error')
+        return user_choice()
+    else:
+        return choice1
+    except ValueError:
+        
+SyntaxError: invalid syntax
+except ValueError:
+    
+SyntaxError: invalid syntax
+try:
+    choice1 = int(choice1)
+    if (choice1 > 8) and (choice1 < 0):
+        print('Error')
+        return user_choice()
+    else:
+        return choice1
+    except ValueError:
+        
+SyntaxError: invalid syntax
+except ValueError:
+    
+SyntaxError: invalid syntax
+except ValueError:
+    
+SyntaxError: invalid syntax
+
+except ValueError:
+    
+SyntaxError: invalid syntax
+try:
+    choice1 = int(choice1)
+    if (choice1 > 8) and (choice1 < 0):
+        print('Error')
+        return user_choice()
+    else:
+        return choice1
+    except ValueError:
+        
+SyntaxError: invalid syntax
+def user_choice():
+   choice1 = input('Введите Ваш выбор: ')
+   try:
+       choice1 = int(choice1)
+       if (choice1 > 8) and (choice1 < 0):
+           print('Error')
+           return user_choice()
+        else:
+            
+SyntaxError: unindent does not match any outer indentation level
+if (choice1 > 5) and (choice1 < 0):
+if (choice1 > 8) and (choice1 < 0):
+    
+SyntaxError: expected an indented block after 'if' statement on line 1
+if (choice1 > 8) and (choice1 < 0):
+    print('Error')
+    return user_choice()
+SyntaxError: incomplete input
+try:
+    choice1 = int(choice1)
+    if (choice1 > 8) and (choice1 < 0):
+        print('Error')
+        return user_choice()
+    else:
+        return choice1
+    except ValueError:
+        
+SyntaxError: invalid syntax
+except ValueError:
+        print('Error')
+        
+SyntaxError: invalid syntax
+try:
+    choice1 = int(choice1)
+    if (choice1 > 8) and (choice1 < 0):
+        print('Error')
+        return user_choice()
+    else:
+        return choice1
+
+
+
+
+except ValueError:
+    print('Error')
+    
+SyntaxError: incomplete input
+print('Error')
+Error
+error_enter()
+Traceback (most recent call last):
+  File "<pyshell#102>", line 1, in <module>
+    error_enter()
+NameError: name 'error_enter' is not defined
+return user_choice()
+SyntaxError: 'return' outside function
+except ValueError:
+    
+SyntaxError: invalid syntax
+try:
+    choice1 = int(choice1)
+    if (choice1 > 8) and (choice1 < 0):
+        print('Error')
+        return user_choice()
+    else:
+        return choice1
+
+
+except ValueError:
+    print('Error')
+    
+SyntaxError: incomplete input
+print('Error')
+Error
+return user_choice()
+SyntaxError: 'return' outside function
+try:
+    choice1 = int(choice1)
+    if (choice1 > 8) and (choice1 < 0):
+        print('Error')
+        return user_choice()
+...     else:
+...         return choice1
+... 
+... 
+... except ValueError:
+...     print('Error')
+...     
+SyntaxError: incomplete input
+>>> print('Error')
+Error
+>>> def data_search():
+...     try:
+...         my_search = input('Введите класс для поиска ученика: ')
+...         except ValueError:
+...             
+SyntaxError: invalid syntax
+>>> except ValueError:
+...     
+SyntaxError: invalid syntax
+>>> search(my_search)
+Traceback (most recent call last):
+  File "<pyshell#122>", line 1, in <module>
+    search(my_search)
+NameError: name 'search' is not defined
+>>> def data_search():
+...     try:
+...         my_search = input('Введите класс для поиска ученика: ')
+...     except ValueError:
+...         print('Error')
+... 
+...         
+>>>     return my_search
+SyntaxError: unexpected indent
+>>> def data_search():
+...     try:
+...         my_search = input('Введите класс для поиска ученика: ')
+...     except ValueError:
+...         print('Error')
+...     return my_search
